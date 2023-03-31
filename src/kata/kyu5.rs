4bdedf028ab3651acbd847acc56ef71f76de6a49
Talandar <talandar66@protonmail.com>
2023-03-31 13:56:35 +0200

    int32 to IPv4

--- src/kata/kyu5.rs
+++ src/kata/kyu5.rs
@@ -4,3 +4,13 @@ pub fn make_readable(seconds: u32) -> String {
     let mut _hours: u32 = seconds / 60 / 60;
     format!("{:02}:{:02}:{:02}", _hours, _minutes, _seconds)
 }
+fn int32_to_ip(int: u32) -> String {
+    let binary_string: String = format!("{int:032b}");
+    format!(
+        "{}.{}.{}.{}",
+        i32::from_str_radix(&binary_string[0..8], 2).expect("Not a Binary Value"),
+        i32::from_str_radix(&binary_string[8..16], 2).expect("Not a Binary Value"),
+        i32::from_str_radix(&binary_string[16..24], 2).expect("Not a Binary Value"),
+        i32::from_str_radix(&binary_string[24..32], 2).expect("Not a Binary Value"),
+    )
+}
