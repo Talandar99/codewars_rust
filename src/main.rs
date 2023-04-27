f901cb69a9ba54f2afe82a3e49718cee6298ce03
Talandar <talandar66@protonmail.com>
2023-04-27 15:00:02 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-#[allow(dead_code)]
-mod kata;
-
 fn main() {
-    println!("{}", kata::kyu8::boolean_to_string(true));
+    print!("{}", binary_slice_to_number(&[0, 0, 1, 0]));
+}
+
+fn binary_slice_to_number(slice: &[u32]) -> u32 {
+    slice.iter().fold(0, |acc, &bit| acc * 2 + bit)
 }
